67372b0fa3d29549c48211438000f383ae0a478f
fhlare <127776539+fhlare@users.noreply.github.com>
2023-09-10 03:29:17 +0300

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const markup = cratePicturesGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+
+function cratePicturesGallery(arr) {
+  return arr.map(({ preview, original, description }) =>
+    `<li class="gallery_item">
+    <a class="gallery__link" href="${original}">
+    <img src="${preview}" alt="${description}" class="gallery__image"/>
+    </a>
+    </li>`
+  ).join("");
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: 'true',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+});
 
 console.log(galleryItems);
