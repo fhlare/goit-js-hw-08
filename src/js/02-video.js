f1491784a60323ae52b366a2e59841c17d699688
fhlare <127776539+fhlare@users.noreply.github.com>
2023-09-11 02:22:58 +0300

    done second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+
+const currentTimeValue = localStorage.getItem('videoplayer-current-time') || 0;
+player.setCurrentTime(currentTimeValue);
