aef0e1178830820a8c012d026deef987bec762e8
fhlare <127776539+fhlare@users.noreply.github.com>
2023-09-13 22:27:22 +0300

    done third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from "lodash.throttle"
 
+const form = document.querySelector('.feedback-form');
+
+const KEY_STORAGE = "feedback-form-state";
+let currentData = {};
+form.addEventListener ('input', throttle(onInput, 500)) ;
+const currentLocalData = JSON.parse(localStorage.getItem(KEY_STORAGE));
+
+if (currentLocalData) {
+  form.email.value = currentLocalData.email;
+  form.message.value = currentLocalData.message;
+};
+  
+  function onInput() {
+    currentData = {
+      email: form.email.value,
+      message: form.message.value,
+    };
+  
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(currentData));
+  };
+
+  
+  form.addEventListener('submit', onSubmit);
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(currentData);
+    localStorage.removeItem(KEY_STORAGE);
+    e.currentTarget.reset();
+};
